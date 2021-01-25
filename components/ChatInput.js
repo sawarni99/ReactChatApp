e4644484099bdc8a16798b08a8c60d9144c9ca0a
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-25 19:49:07 +0530

    Added chat box in chat room

---
+++ components/ChatInput.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const ChatInput = ({ onChangeText, placeholder, onSubmit }) => {
+  return (
+    <View style={styles.chatInputContainer}>
+      <TextInput
+        style={styles.chatInput}
+        multiline
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        placeholderTextColor="gray"
+      />
+      <TouchableOpacity style={styles.chatSendIcon} onPress={onSubmit}>
+        <Ionicons name="md-send-sharp" size={30} color="whitesmoke" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ChatInput;
+
+const styles = StyleSheet.create({
+  chatInputContainer: {
+    borderWidth: 2,
+    borderColor: "gray",
+    width: "100%",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+  chatInput: {
+    width: "100%",
+    height: 45,
+    flex: 1,
+    padding: 10,
+    fontSize: 15,
+    color: "whitesmoke",
+  },
+  chatSendIcon: {
+    padding: 10,
+    justifyContent: "center",
+  },
+});
--- pages/Chat.js
+++ pages/Chat.js
@@ -1,15 +1,46 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import ChatMessage from "../components/ChatMessage";
-import Input from "../components/CustomInput";
+import Input from "../components/ChatInput";
 
 const Chat = () => {
+  // Sample messages....
+  const listMessages = [
+    { key: "1", sender: "you", message: "hii how are you" },
+    { key: "2", sender: "gopi", message: "I am fine how are you" },
+    {
+      key: "3",
+      sender: "you",
+      message: "I will say blah blah then please replay blah blah....",
+    },
+    { key: "4", sender: "gopi", message: "OK I will reply blah blah..." },
+    {
+      key: "5",
+      sender: "you",
+      message: "My name is gopi bahu... say hello to me",
+    },
+    { key: "6", sender: "gopi", message: "Hello" },
+    {
+      key: "7",
+      sender: "you",
+      message:
+        "Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah !!!!",
+    },
+  ];
+
   return (
     <View style={styles.chat}>
       <View style={styles.chatDetails}>
         <Text style={styles.chatName}>Gopi Bahu</Text>
       </View>
-      <View style={styles.chatArea}></View>
+      <View style={styles.chatArea}>
+        <FlatList
+          data={listMessages}
+          renderItem={({ item }) => (
+            <ChatMessage sender={item.sender} message={item.message} />
+          )}
+        />
+      </View>
       <View style={styles.chatBox}>
         <Input placeholder="Enter a message to send..." />
       </View>
@@ -27,9 +58,8 @@ const styles = StyleSheet.create({
   },
   chatDetails: {
     backgroundColor: "white",
-    height: "8%",
+    height: 40,
     opacity: 0.7,
-
     justifyContent: "center",
     alignItems: "center",
   },
@@ -38,12 +68,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   chatArea: {
-    height: "78%",
+    flex: 1,
+    marginBottom: 75,
   },
   chatBox: {
-    height: "15%",
+    height: 70,
     padding: 10,
     justifyContent: "center",
     alignItems: "center",
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
   },
 });
