ee822632f4bfa68741d35a778e12e1512344678c
Sawarni Swaroop <sawarni99@gmail.com>
2021-01-25 21:51:42 +0530

    Added react redux

--- App.js
+++ App.js
@@ -2,41 +2,44 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "react-redux";
+import store from "./reducers/rootReducer";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import BackButton from "./components/BackButton";
 
 export default function App() {
   const Stack = createStackNavigator();
-
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              elevation: 10,
-              opacity: 0.95,
-              backgroundColor: "#6930c3",
-              height: 90,
-            },
-            headerTintColor: "whitesmoke",
-            headerTitleStyle: {
-              fontSize: 25,
-            },
-          }}
-        >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen
-            options={({ navigation }) => ({
-              headerLeft: () => <BackButton navigation={navigation} />,
-            })}
-            name="Chat"
-            component={Chat}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: {
+                elevation: 10,
+                opacity: 0.95,
+                backgroundColor: "#6930c3",
+                height: 90,
+              },
+              headerTintColor: "whitesmoke",
+              headerTitleStyle: {
+                fontSize: 25,
+              },
+            }}
+          >
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen
+              options={({ navigation }) => ({
+                headerLeft: () => <BackButton navigation={navigation} />,
+              })}
+              name="Chat"
+              component={Chat}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </Provider>
   );
 }
 
---
+++ reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from "redux";
+
+const AppReducers = combineReducers();
+
+const rootReducer = (state, action) => {
+  return AppReducers(state, action);
+};
+
+const store = createStore(rootReducer);
+
+export default store;
